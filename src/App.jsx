70a52e699b8c37eb3a4334fcb3012386f1cebf16
kat-lev <kat@katleverton.com>
2025-03-18 19:18:22 +0100

    feat(RV-28): Create basic footer

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 /**
  * App component that manages the routing for the application.
@@ -33,6 +34,7 @@ function App() {
           <Route path="/" element={<div>Hola mundo</div>} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+        <footer className="text-emerald-900 bg-green-100 flex justify-between font-['Fira_Sans']">
+            <div className="flex align-items-start items-center p-2">
+                <p>ReVive. Derechos reservados.</p>     
+            </div>
+            <div className="flex items-end p-2">
+                <ul className="flex items-center space-x-1">
+                    <li>
+                        <p>Política de privacidad</p> 
+                    </li>
+                    <li>
+                        <p >Condiciones</p> 
+                    </li>
+                </ul>
+            </div> 
+        </footer>    
+    );
+}
+
+export default Footer
