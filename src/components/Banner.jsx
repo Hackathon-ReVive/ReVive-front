16a55a51bbc76f414d544d292d5bbc5fa3ee9a34
Algol95 <angelaragon.developer@gmail.com>
2025-03-20 12:08:08 +0100

    feat(RV-41): Added Styles Figma

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,19 @@
-import banner from "./../assets/banner-image.png";
+import banner from "/assets/banner-image.png";
 
 function Banner() {
-    return (
-        <section className="bg-emerald-900 p-10 w-full flex flex-col md:flex-row">
-            <div className="p-10 flex flex-col justify-center align-start md:m-0 md:w-1/2">
-                <h1 className = "text-[50px] text-emerald-100 font-['Playfair_Display']">
-                    Explora nuestros productos
-                </h1>
-                <button className="flex align-center">
-                    <p>
-                        Empieza ahora
-                    </p>
-                </button>
-            </div>
-            <img src={banner} alt="foto-persona-trabajando-con-su-portatil">
-            </img>
-        </section>
-    );
+  return (
+    <section className="bg-emerald-900 p-10 w-full flex flex-col md:flex-row">
+      <div className="p-10 flex flex-col justify-center align-start md:m-0 md:w-1/2">
+        <h1 className="text-[50px] text-emerald-100 font-['Playfair_Display']">
+          Explora nuestros productos
+        </h1>
+        <button className="flex align-center">
+          <p>Empieza ahora</p>
+        </button>
+      </div>
+      <img src={banner} alt="foto-persona-trabajando-con-su-portatil"></img>
+    </section>
+  );
 }
 
-export default Banner
+export default Banner;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -25,52 +25,75 @@ function LoginForm() {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-50 p-6 bg-white shadow-md rounded-md border-1 border-stone-200">
-      <h2 className="text-2xl font-semibold text-center text-gray-700 mb-4">
-        Login
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <input
-          type="email"
-          {...register("email", {
-            required: "Email es obligatorio",
-            pattern: {
-              value: /^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$/i,
-              message: "Formato de email inválido",
-            },
-          })}
-          placeholder="Email Address"
-          className="w-full p-2 border border-gray-300 rounded"
+    <section className="min-h-screen flex items-center justify-center bg-cover bg-center bg-[url(/assets/bgLoginRegister.png)]">
+      <div className="max-w-md mx-auto p-8 bg-[#ECFDF5] shadow-md rounded-3xl border border-green-300 backdrop-blur-lg bg-opacity-80">
+        <h2 className="text-3xl font-bold text-center text-[#064E3B] mb-2">
+          Conéctate
+        </h2>
+        <p className="text-center text-[#059669] mb-4">
+          ¡Dale vida nueva a tus productos!
+        </p>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <label
+            htmlFor="email"
+            className="block text-[#059669] text-sm font-medium"
+          >
+            Correo Electrónico
+          </label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Email es obligatorio",
+              pattern: {
+                value: /^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$/i,
+                message: "Formato de email inválido",
+              },
+            })}
+            placeholder="Email Address"
+            className="w-full p-3 border border-[#A7F3D0] rounded-lg"
+            id="email"
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm border-1 border-red-500 rounded bg-red-100 p-2">
+              {errors.email.message}
+            </p>
+          )}
+
+          <label
+            htmlFor="password"
+            className="block text-[#059669] text-sm font-medium"
+          >
+            Contraseña
+          </label>
+          <input
+            type="password"
+            {...register("password", { required: "Contraseña es obligatoria" })}
+            placeholder="Password"
+            className="w-full p-3 border border-[#A7F3D0] rounded-lg bg-[#ECFDF5]"
+            id="password"
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm border-1 border-red-500 rounded bg-red-100 p-2">
+              {errors.password.message}
+            </p>
+          )}
+
+          <Button
+            type="submit"
+            text="Continuar con Email"
+            className="w-full text-white rounded-lg transition"
+          />
+        </form>
+        <Button
+          text="No tengo cuenta"
+          className="mt-4 border-[#A7F3D0] border w-full rounded-lg"
+          bgColor="bg-[#DCFCE7]"
+          txtColor="text-teal-600"
+          onClick={() => navigate("/register")}
         />
-        {errors.email && (
-          <p className="text-red-500 text-sm border-1 border-red-500 rounded bg-red-100 p-2">
-            {errors.email.message}
-          </p>
-        )}
-
-        <input
-          type="password"
-          {...register("password", { required: "Contraseña es obligatoria" })}
-          placeholder="Password"
-          className="w-full p-2 border border-gray-300 rounded"
-        />
-        {errors.password && (
-          <p className="text-red-500 text-sm border-1 border-red-500 rounded bg-red-100 p-2">
-            {errors.password.message}
-          </p>
-        )}
-
-        <Button type="submit" text="Conectame" className="w-full" />
-      </form>
-
-      <Button
-        text="No tengo cuenta"
-        className="mt-4 border-teal-500 border-1 w-full"
-        bgColor="bg-teal-200"
-        txtColor="text-teal-600"
-        onClick={() => navigate("/register")}
-      />
-    </div>
+      </div>
+    </section>
   );
 }
 
