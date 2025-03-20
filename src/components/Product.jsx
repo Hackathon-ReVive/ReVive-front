a08026b8984f625b2b1b964dd9c04644e796fb34
kat-lev <kat@katleverton.com>
2025-03-20 18:47:35 +0100

    feat(RV-48): Add Buy button and add products to cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../CartContext";
+import Button from "./Button";
 
 function Product({ id, name, image, price, type, description, sold }) {
+    const { addToCart } = useContext(CartContext);
+
     const euroPrice = new Intl.NumberFormat("es-ES", {
         style: "currency",
         currency: "EUR",
@@ -11,19 +15,26 @@ function Product({ id, name, image, price, type, description, sold }) {
     };
 
     return (
-        <article className="bg-white rounded-lg p-4 transition duration-300">
+        <article className="bg-white rounded-lg p-4 transition duration-300 relative">
             <img src={image} alt={name} className="w-full h-40 object-cover rounded-lg aspect-[2/3]" />
             <h2 className="font-['Fira_Sans'] text-lg font-semibold mt-2 text-emerald-900">{name}</h2>
             <p className="font-['Fira_Sans'] text-black">{type}</p>
             <p className="font-['Fira_Sans'] text-emerald-900 font-bold">{euroPrice}</p>
             <p className="font-['Fira_Sans'] text-black mt-1">{description}</p>
-                {sold && (
-                    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-bold">
+
+            {sold ? (
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-bold">
                     ¡Vendido!
-                    </div>
-                )}
+                </div>
+            ) : (
+                <Button
+                    text="Agregar al carrito"
+                    onClick={() => addToCart({ id, name, image, price, type, description })}
+                    className="mt-2 bg-emerald-900 text-white w-full"
+                />
+            )}
         </article>
     );
 }
 
-export default Product
+export default Product;
