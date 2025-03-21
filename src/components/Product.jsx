e49520b3be8be9f2112b4c3d83649745f53d46d1
Algol95 <angelaragon.developer@gmail.com>
2025-03-21 02:11:14 +0100

    Merge branch 'development' of https://github.com/Hackathon-ReVive/ReVive-front into development

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,54 +1,50 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { CartContext } from "../CartContext";
 import Button from "./Button";
-import Modal from "./Modal";
-import ProductDetail from "./ProductDetail";
 
-function Product({ id, name, image, price, type, description, sold }) {
-    const { addToCart } = useContext(CartContext);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+function Product({ id, title, image, price, type, description, sold }) {
+  const { addToCart } = useContext(CartContext);
 
-    const euroPrice = new Intl.NumberFormat("es-ES", {
-        style: "currency",
-        currency: "EUR",
-    }).format(price);
+  const euroPrice = new Intl.NumberFormat("es-ES", {
+    style: "currency",
+    currency: "EUR",
+  }).format(price);
 
-    Product.defaultProps = {
-        sold: false,
-    };
+  Product.defaultProps = {
+    sold: false,
+  };
 
-    return (
-        <article className="bg-white rounded-lg p-4 transition duration-300 relative">
-            {/* Clickable Image that opens the modal */}
-            <img 
-                src={image} 
-                alt={name} 
-                className="w-full h-40 object-cover rounded-lg aspect-[2/3] cursor-pointer"
-                onClick={() => setIsModalOpen(true)} 
-            />
-            <h2 className="font-['Fira_Sans'] text-lg font-semibold mt-2 text-emerald-900">{name}</h2>
-            <p className="font-['Fira_Sans'] text-black">{type}</p>
-            <p className="font-['Fira_Sans'] text-emerald-900 font-bold">{euroPrice}</p>
-            <p className="font-['Fira_Sans'] text-black mt-1">{description}</p>
+  return (
+    <article className="bg-white rounded-lg p-4 transition duration-300 relative">
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-40 object-cover rounded-lg aspect-[2/3]"
+      />
+      <h2 className="font-['Fira_Sans'] text-lg font-semibold mt-2 text-emerald-900">
+        {title}
+      </h2>
+      <p className="font-['Fira_Sans'] text-black">{type}</p>
+      <p className="font-['Fira_Sans'] text-emerald-900 font-bold">
+        {euroPrice}
+      </p>
+      <p className="font-['Fira_Sans'] text-black mt-1">{description}</p>
 
-            {sold ? (
-                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-bold">
-                    ¡Vendido!
-                </div>
-            ) : (
-                <Button
-                    text="Agregar al carrito"
-                    onClick={() => addToCart({ id, name, image, price, type, description })}
-                    className="mt-2 bg-emerald-900 text-white w-full"
-                />
-            )}
-
-
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <ProductDetail product={{ id, name, image, price, type, description }} showAddToCart={false} />
-            </Modal>
-        </article>
-    );
+      {sold ? (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-lg font-bold">
+          ¡Vendido!
+        </div>
+      ) : (
+        <Button
+          text="Agregar al carrito"
+          onClick={() =>
+            addToCart({ id, title, image, price, type, description })
+          }
+          className="mt-2 bg-emerald-900 text-white w-full"
+        />
+      )}
+    </article>
+  );
 }
 
 export default Product;
