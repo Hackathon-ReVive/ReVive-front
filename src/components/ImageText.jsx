99f8e22077e9ffae3199eaf5776af92a5b282d0e
Algol95 <angelaragon.developer@gmail.com>
2025-03-21 04:50:44 +0100

    Fixed Image-text

--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -1,12 +1,12 @@
 function ImageText() {
   return (
-    <section className="relative">
+    <section className="relative flex items-center justify-center">
       <img
         src="/assets/text-image.png"
-        className="z-0 w-full opacity-75"
+        className="absolute inset-0 z-0 w-full h-full object-cover opacity-75"
         alt="foto estética de un salón con vista atractiva"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 max-w-[1096px] text-center justify-start text-white text-7xl font-extrabold font-[Playfair_Display] leading-[77.76px]">
+      <div className="relative z-10 max-w-[1096px] text-center text-white text-2xl md:text-4xl lg:text-7xl font-extrabold font-[Playfair_Display] px-4 py-8 lg:py-16 leading-tight lg:leading-[77.76px]">
         Nuestra misión es crear una cultura que le da una segunda vida a los
         productos
       </div>
